0b35a9a602c4b66f8ba7451a74b6c9308b17e4aa
shurona <khc4669@naver.com>
2025-04-18 17:10:41 +0900

    refactor: root 폴더 이름 변경

---
+++ chat-program/src/main/java/com/shurona/chat/mytalk/user/presentation/controller/FriendController.java
@@ -0,0 +1,10 @@
+package com.shurona.chat.mytalk.user.presentation.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/friends/v1")
+@Controller
+public class FriendController {
+
+}
