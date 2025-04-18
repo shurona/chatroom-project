e01a20481ac8b22d589844885ac50d89010bef16
shurona <khc4669@naver.com>
2025-04-18 16:43:06 +0900

    feat: 공통 static 처리

---
+++ user/src/main/java/com/shurona/chat/mytalk/common/variable/StaticVariable.java
@@ -0,0 +1,9 @@
+package com.shurona.chat.mytalk.common.variable;
+
+public class StaticVariable {
+
+    public final static String LOGIN_USER = "login-session";
+
+    public final static int LOGIN_SESSION_TIME = 30 * 60;
+
+}
