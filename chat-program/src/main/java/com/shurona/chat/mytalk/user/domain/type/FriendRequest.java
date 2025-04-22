e787902d4c094b2181494ee06abe74814f08cf18
shurona <khc4669@naver.com>
2025-04-22 20:08:28 +0900

    feat: friend repo

--- chat-program/src/main/java/com/shurona/chat/mytalk/user/domain/type/FriendRequest.java
+++ chat-program/src/main/java/com/shurona/chat/mytalk/user/domain/type/FriendRequest.java
@@ -6,14 +6,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum FriendRequest {
-    REQUESTED(RequestStatus.REQUEST),
-    REFUSED(RequestStatus.REFUSED);
 
+    REQUESTED(RequestStatus.REQUEST),
+    ACCEPTED(RequestStatus.ACCEPTED),
+    REFUSED(RequestStatus.REFUSED);
+//
     private final String status;
 
     public static class RequestStatus {
 
         public static final String REQUEST = "FRIEND_REQUEST";
+        public static final String ACCEPTED = "FRIEND_ACCEPTED";
         public static final String REFUSED = "FRIEND_REFUSED";
+
     }
 }
