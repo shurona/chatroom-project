2a6e8c73f213cb2bd529b3681feaa0c24736683f
shurona <khc4669@naver.com>
2025-04-29 20:51:25 +0900

    feat: global exception basic

---
+++ chat-program/src/main/java/com/shurona/chat/mytalk/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.shurona.chat.mytalk.common.exception;
+
+import com.shurona.chat.mytalk.chat.common.ChatErrorCode;
+import com.shurona.chat.mytalk.chat.common.ChatException;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ChatException.class)
+    public String handleChatError(ChatException e, Model model) {
+        ChatErrorCode code = e.getCode();
+        model.addAttribute("errorMessage", code.getMessage());
+        model.addAttribute("status", code.getStatus());
+        return "error/customError"; //
+    }
+
+}
