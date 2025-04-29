8b88c3bcaa2be4963692f95a64554dc8df07ef1d
shurona <khc4669@naver.com>
2025-04-29 20:50:04 +0900

    feat: chat 서비스

---
+++ chat-program/src/main/java/com/shurona/chat/mytalk/chat/application/ChatService.java
@@ -0,0 +1,53 @@
+package com.shurona.chat.mytalk.chat.application;
+
+import com.shurona.chat.mytalk.chat.domain.model.ChatLog;
+import com.shurona.chat.mytalk.chat.domain.model.ChatRoom;
+import com.shurona.chat.mytalk.chat.domain.type.ChatContentType;
+import com.shurona.chat.mytalk.chat.domain.type.RoomType;
+import com.shurona.chat.mytalk.chat.presentation.dtos.ChatLogResponseDto;
+import com.shurona.chat.mytalk.user.domain.model.User;
+import java.util.List;
+import org.springframework.data.domain.Pageable;
+
+public interface ChatService {
+
+    public ChatRoom createChatRoom(User user, List<User> invitedUserList, RoomType type,
+        String names);
+
+    /*
+        id를 기준으로 채팅방 단일 조회
+     */
+    public ChatRoom findChatRoomById(Long id);
+
+    /*
+        내가 속한 채팅방 목록을 갖고 온다.
+     */
+    public List<ChatRoom> findChatRoomListByUser(User user);
+
+    /*
+        채팅 입력
+     */
+    public ChatLog writeChat(ChatRoom room, User user, String chatData, ChatContentType type);
+
+    /*
+        채팅 목록 조회
+     */
+    public List<ChatLogResponseDto> readChatLog(User user, ChatRoom room, Pageable pageable);
+
+    /*
+    ------------------------------------------------------------------------------
+    ****************   그룹 채팅 방 관련  메소드   **********************
+    ------------------------------------------------------------------------------
+     */
+
+    /*
+        채팅 창에 초대
+     */
+    public ChatRoom inviteUser(User user, User friend, ChatRoom room);
+
+    /*
+        채팅 방에서 친구 밴
+     */
+//    public ChatRoom bannedUser(User user, User friend, ChatRoom room);
+
+}
