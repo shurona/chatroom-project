cb7107dfa1955120323415e4d27eb8a0c3d8673f
shurona <khc4669@naver.com>
2025-05-01 20:53:38 +0900

    feat: 단순 주석

--- chat-program/src/main/java/com/shurona/chat/mytalk/user/service/UserService.java
+++ chat-program/src/main/java/com/shurona/chat/mytalk/user/service/UserService.java
@@ -12,6 +12,9 @@ public interface UserService {
 
     public boolean checkPasswordCorrect(String inputPassword, String dbPassword);
 
+    /**
+     * 유저 ID를 기준으로 유저 검색(없으면 자동 에러 처리)
+     */
     public User findUserById(Long userId);
 
     public User findUserByLoginId(String loginId);
