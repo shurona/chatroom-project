129e1af5da29188aada0770cf79a985fd488c804
shurona <khc4669@naver.com>
2025-06-22 17:01:24 +0900

    test: ci 수정

---
+++ chat-program/src/test/java/com/shurona/chat/mytalk/config/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.shurona.chat.mytalk.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@TestConfiguration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
