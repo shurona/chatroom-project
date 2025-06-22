dd31e07ea8705b5333d0c8e2d21b6189057d9ce3
shurona <khc4669@naver.com>
2025-06-22 18:59:06 +0900

    feat: admin controller 추가

--- chat-program/src/main/java/com/shurona/chat/mytalk/admin/presentation/AdminController.java
+++ chat-program/src/main/java/com/shurona/chat/mytalk/admin/presentation/AdminController.java
@@ -1,5 +1,11 @@
 package com.shurona.chat.mytalk.admin.presentation;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class AdminController {
 
+    @GetMapping("admin/yahoo")
+    public void yahoo() {
+
+    }
 }
