78a7e8415624d00c8aba48d10652e7475b88e423
shurona <khc4669@naver.com>
2025-06-29 17:40:48 +0900

    feat: 현재 User에 맞게 변경

--- chat-program/src/main/java/com/shurona/chat/mytalk/user/presentation/dto/request/UserCreateRequestDto.java
+++ chat-program/src/main/java/com/shurona/chat/mytalk/user/presentation/dto/request/UserCreateRequestDto.java
@@ -2,7 +2,7 @@ package com.shurona.chat.mytalk.user.presentation.dto.request;
 
 public record UserCreateRequestDto(
     String loginId,
-    String username, // 유저 닉네임 일단 지금은 사용하지 않음.
+    String nickName,
     String password,
     String description,
     String phoneNumber
