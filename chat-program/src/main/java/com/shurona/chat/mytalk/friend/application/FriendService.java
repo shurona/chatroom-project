e22e25dfb2689eb29332111040bcdca7ee5a7eb0
shurona <khc4669@naver.com>
2025-06-29 17:34:32 +0900

    feat: Friend 생성 로직 보완 및 목록 기준 조회 로직 추가

--- chat-program/src/main/java/com/shurona/chat/mytalk/friend/application/FriendService.java
+++ chat-program/src/main/java/com/shurona/chat/mytalk/friend/application/FriendService.java
@@ -4,34 +4,46 @@ import com.shurona.chat.mytalk.friend.domain.model.Friend;
 import com.shurona.chat.mytalk.friend.domain.model.type.FriendRequest;
 import com.shurona.chat.mytalk.user.domain.model.User;
 import java.util.List;
+import java.util.Map;
 
 public interface FriendService {
 
-    /*
-        단일 조회
+    /**
+     * 단일 조회
      */
     public Friend findFriendById(Long id);
 
-    /*
-        친구 저장(요청으로 저장)
+    /**
+     * 친구 저장(요청으로 저장)
      */
     public Friend saveFriend(User user, User friend);
 
-    /*
-        유저를 기준으로 받아들인 친구 목록을 받아오기
-        // TODO: 패이징 추가
+    /**
+     * 친구 요청 승인 시 상대도 자동승인을 위한 함수
+     */
+    public Friend saveAcceptFriend(User user, User friend);
+
+    /**
+     * 유저를 기준으로 받아들인 친구 목록을 받아오기
+     * // TODO: 패이징 추가
      */
     public List<Friend> findAcceptedFriendListByUser(User user);
 
-    /*
-        유저를 기준으로 친구 요청 목록 받아오기
-        // TODO: 패이징 추가
+    /**
+     * 유저를 기준으로 친구 요청 목록 받아오기
+     * // TODO: 패이징 추가
      */
     public List<Friend> findRequestedFriendListByUser(User user);
 
+    /**
+     * 유저를 기준으로 친구에 해당하는 유저 목록을 기준으로 목록을 조회하기
+     */
+    public Map<Long, FriendRequest> findRequestFriendStatusByUserAndFriendIds(
+        User user, List<User> friendList);
 
-    /*
-        친구 상태 변경
+
+    /**
+     * 친구 상태 변경
      */
     public Friend changeStatusById(Long friendId, FriendRequest status);
 
