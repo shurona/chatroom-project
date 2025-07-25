0505742cdbfe5fef2e093b462b6b1f69af421cd6
shurona <khc4669@naver.com>
2025-07-25 16:01:55 +0900

    feat: 소켓 연결에서 세션 정보를 추가

--- chat-program/src/main/java/com/shurona/chat/mytalk/common/interceptor/SocketInterceptor.java
+++ chat-program/src/main/java/com/shurona/chat/mytalk/common/interceptor/SocketInterceptor.java
@@ -1,5 +1,11 @@
 package com.shurona.chat.mytalk.common.interceptor;
 
+import com.shurona.chat.mytalk.chat.common.exception.ChatErrorCode;
+import com.shurona.chat.mytalk.chat.common.exception.ChatException;
+import com.shurona.chat.mytalk.user.common.utils.JwtUtils;
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
@@ -8,19 +14,48 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
+@Slf4j
 @Component
 public class SocketInterceptor implements ChannelInterceptor {
+
+    private final JwtUtils jwtUtils;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
 
-        if (StompCommand.SUBSCRIBE.equals(accessor.getCommand())) {
-            String authHeader = accessor.getFirstNativeHeader(HttpHeaders.AUTHORIZATION);
+        log.info("이건 정말인가요 , 연결 상태 {}", accessor.getCommand());
 
-            //TODO: 이런 곳에서 접근을 막을 수 있을까
+        // 연결 할 때 인증 정보를 등록해준다.
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            // 토큰 추출
+            String token = jwtUtils.substringToken(
+                accessor.getFirstNativeHeader(HttpHeaders.AUTHORIZATION)
+            );
+
+            if (!jwtUtils.checkValidJwtToken(token)) {
+                throw new ChatException(ChatErrorCode.PRIVATE_CHAT_JUST_ONE_REQUIRED);
+            }
+
+            Claims bodyFromJwt = jwtUtils.getBodyFromJwt(token);
+
+            long userId = Long.parseLong(bodyFromJwt.getSubject()); // 사용자 ID 추출
+            String role = bodyFromJwt.get(JwtUtils.AUTHORIZATION_KEY,
+                String.class); // 사용자 권한 추출
+
+            accessor.setUser(() -> String.valueOf(userId));
+            // 세션 속성에도 저장
+            accessor.getSessionAttributes().put("userId", userId);
+
         } else if (StompCommand.SEND.equals(accessor.getCommand())) {
-            // send 접근
+            // 세션에서 userId 가져와서 Principal 설정
+            Object userId = accessor.getSessionAttributes().get("userId");
+            if (userId != null) {
+                accessor.setUser(() -> String.valueOf(userId));
+            } else {
+                throw new ChatException(ChatErrorCode.SOCKET_NOT_AUTHORIZATION);
+            }
         }
 
         return message;
