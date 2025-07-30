09cfcbf05247a5e85e045a0ebcb01f13d8de2225
shurona <khc4669@naver.com>
2025-07-30 18:20:18 +0900

    feat: interceptor에 에러 핸들링 추가

--- chat-program/src/main/java/com/shurona/chat/mytalk/common/interceptor/SocketInterceptor.java
+++ chat-program/src/main/java/com/shurona/chat/mytalk/common/interceptor/SocketInterceptor.java
@@ -2,6 +2,8 @@ package com.shurona.chat.mytalk.common.interceptor;
 
 import com.shurona.chat.mytalk.chat.common.exception.ChatErrorCode;
 import com.shurona.chat.mytalk.chat.common.exception.ChatException;
+import com.shurona.chat.mytalk.user.common.exception.UserErrorCode;
+import com.shurona.chat.mytalk.user.common.exception.UserException;
 import com.shurona.chat.mytalk.user.common.utils.JwtUtils;
 import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
@@ -19,46 +21,77 @@ import org.springframework.stereotype.Component;
 @Component
 public class SocketInterceptor implements ChannelInterceptor {
 
+    private static final String USER_ID_KEY = "userId";
     private final JwtUtils jwtUtils;
 
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        StompCommand command = accessor.getCommand();
 
-        log.info("이건 정말인가요 , 연결 상태 {}", accessor.getCommand());
+        System.out.println(accessor);
 
-        // 연결 할 때 인증 정보를 등록해준다.
-        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
-            // 토큰 추출
-            String token = jwtUtils.substringToken(
-                accessor.getFirstNativeHeader(HttpHeaders.AUTHORIZATION)
-            );
-
-            if (!jwtUtils.checkValidJwtToken(token)) {
-                throw new ChatException(ChatErrorCode.INVALID_JWT_TOKEN);
-            }
-
-            Claims bodyFromJwt = jwtUtils.getBodyFromJwt(token);
-
-            long userId = Long.parseLong(bodyFromJwt.getSubject()); // 사용자 ID 추출
-            String role = bodyFromJwt.get(JwtUtils.AUTHORIZATION_KEY,
-                String.class); // 사용자 권한 추출
-
-            accessor.setUser(() -> String.valueOf(userId));
-            // 세션 속성에도 저장
-            accessor.getSessionAttributes().put("userId", userId);
-
-        } else if (StompCommand.SEND.equals(accessor.getCommand())) {
-            // 세션에서 userId 가져와서 Principal 설정
-            Object userId = accessor.getSessionAttributes().get("userId");
-            if (userId != null) {
-                accessor.setUser(() -> String.valueOf(userId));
-            } else {
-                throw new ChatException(ChatErrorCode.SOCKET_NOT_AUTHORIZATION);
-            }
+        if (StompCommand.CONNECT.equals(command)) {
+            handleConnect(accessor);
+        } else if (StompCommand.SEND.equals(command)) {
+            handleSend(accessor);
+        } else if (StompCommand.DISCONNECT.equals(command)) {
+            handleDisconnect(accessor);
         }
 
         return message;
     }
 
+    private void handleConnect(StompHeaderAccessor accessor) {
+        log.info("연결되었습니다. , 연결 상태 {}", accessor.getCommand());
+
+        String token = extractToken(accessor);
+        validateToken(token);
+
+        Claims claims = jwtUtils.getBodyFromJwt(token);
+        long userId = extractUserId(claims);
+
+        setUserAuthentication(accessor, userId);
+    }
+
+    private String extractToken(StompHeaderAccessor accessor) {
+        try {
+            return jwtUtils.substringToken(
+                accessor.getFirstNativeHeader(HttpHeaders.AUTHORIZATION)
+            );
+        } catch (Exception e) {
+            throw new UserException(UserErrorCode.JWT_TOKEN_INVALID);
+        }
+    }
+
+    // token의 validation을 처리한다.
+    private void validateToken(String token) {
+        if (!jwtUtils.checkValidJwtToken(token)) {
+            throw new ChatException(ChatErrorCode.INVALID_JWT_TOKEN);
+        }
+    }
+
+    // JwtClaim에서 UserId를 추출한다.
+    private long extractUserId(Claims claims) {
+        return Long.parseLong(claims.getSubject());
+    }
+
+    //
+    private void setUserAuthentication(StompHeaderAccessor accessor, long userId) {
+        accessor.setUser(() -> String.valueOf(userId));
+        accessor.getSessionAttributes().put(USER_ID_KEY, userId);
+    }
+
+    private void handleSend(StompHeaderAccessor accessor) {
+        Object userId = accessor.getSessionAttributes().get(USER_ID_KEY);
+        if (userId != null) {
+            accessor.setUser(() -> String.valueOf(userId));
+        } else {
+            throw new ChatException(ChatErrorCode.SOCKET_NOT_AUTHORIZATION);
+        }
+    }
+
+    private void handleDisconnect(StompHeaderAccessor accessor) {
+        log.info("연결 해제되었습니다. , 연결 상태 {}", accessor.getCommand());
+    }
 }
