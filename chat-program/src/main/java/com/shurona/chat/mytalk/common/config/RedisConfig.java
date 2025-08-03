465c07eaa9421bd7fbbfabd1f954d53c741b6842
shurona <khc4669@naver.com>
2025-08-03 14:30:06 +0900

    feat: Redis에 Template과 Mapper 추가

--- chat-program/src/main/java/com/shurona/chat/mytalk/common/config/RedisConfig.java
+++ chat-program/src/main/java/com/shurona/chat/mytalk/common/config/RedisConfig.java
@@ -1,5 +1,9 @@
 package com.shurona.chat.mytalk.common.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.shurona.chat.mytalk.common.variable.RedisProperty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.EnableCaching;
@@ -9,7 +13,9 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @RequiredArgsConstructor
 @EnableCaching
@@ -19,9 +25,24 @@ public class RedisConfig {
     private final RedisProperty redisProperty;
 
     @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        System.out.println("redisProperty = " + redisProperty);
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
 
+        // Java 8 시간 타입 지원
+        mapper.registerModule(new JavaTimeModule());
+        mapper.registerModule(new Jdk8Module());
+
+        // 날짜를 ISO-8601 문자열로 저장
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        // 알 수 없는 속성 무시
+//        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        return mapper;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
         RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
 
         config.setHostName(redisProperty.host());
@@ -32,6 +53,26 @@ public class RedisConfig {
         return new LettuceConnectionFactory(config);
     }
 
+    /**
+     * RedisTemplate을 생성하여 Redis 연결 및 직렬화 방식을 설정합니다.
+     * Key는 String으로, Value는 Jackson을 이용한 JSON(String.class)으로 직렬화합니다.
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+
+        Jackson2JsonRedisSerializer<Object> jsonSerializer =
+            new Jackson2JsonRedisSerializer<>(objectMapper(), Object.class);
+
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(jsonSerializer);
+        return redisTemplate;
+    }
+
+    /**
+     * String to String Redis Template
+     */
     //    @Bean
     public RedisTemplate<String, String> stringRedisTemplate() {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
@@ -48,5 +89,4 @@ public class RedisConfig {
 
         return redisTemplate;
     }
-
 }
